8ae66e4b6779e7351623fd29ea68d8e90632ee7a
Rafiqul <rafiq@gmail.com>
2024-01-17 19:49:23 -0600

    My changed

---
+++ SecoundClassname.java
@@ -0,0 +1,29 @@
+package secoundPackage;
+
+import org.testng.annotations.Test;
+
+public class SecoundClassname {
+	
+	
+	
+	@Test
+	public void testcase4() {
+	System.out.println("case:4r");
+	}
+	
+	@Test
+	public void testcase5() {
+	System.out.println("case:5r");
+	}
+	
+	@Test
+	public void testcase6() {
+	System.out.println("case:6r");
+	}
+	
+	
+	
+	
+	
+
+}
